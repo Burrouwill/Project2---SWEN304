992bd92384f8f521aa1c1a33fa7537c0c5a58ef7
Burrouwill <wgr.burroughs@gmail.com>
2023-06-02 15:11:26 +1200

    Connection to Server seems to work & Driver also

--- src/LibraryModel.java
+++ src/LibraryModel.java
@@ -5,16 +5,47 @@
  */
 
 
-
+import java.sql.*;
 import javax.swing.*;
 
 public class LibraryModel {
 
     // For use in creating dialogs and making them modal
     private JFrame dialogParent;
+    
+    // Connection
+    private Connection con = null;
 
     public LibraryModel(JFrame parent, String userid, String password) {
 	dialogParent = parent;
+	
+	// Register a PostgreSQL Driver
+	try{
+		Class.forName("org.postgresql.Driver");
+		}
+		catch (ClassNotFoundException cnfe){
+			// Change this text
+		System.out.println("Can not find"+
+		"the driver class: "+
+		"\nEither I have not installed it"+
+		"properly or \n postgresql.jar "+
+		" file is not in my CLASSPATH");
+		}
+	
+	
+	// Establish a Connection
+	String url = "jdbc:postgresql://localhost:5432/burrouwill_jdbc";
+
+			try{
+			con = DriverManager.getConnection(url,
+			userid, password);
+			}
+			catch (SQLException sqlex){
+			System.out.println("Can not connect");
+			System.out.println(sqlex.getMessage());
+			}
+	
+	
     }
 
     public String bookLookup(int isbn) {
